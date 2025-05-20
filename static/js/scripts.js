c20fcbdbd0d7bd6b571a65716c762b52ce2c2e56
Musyrl1312 <135121912+syahrial1312@users.noreply.github.com>
2025-05-20 15:24:34 +0700

    Delete static directory

--- static/js/scripts.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-const sidebarToggler = document.querySelector('.sidebar-toggler');
-
-sidebarToggler.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-});
